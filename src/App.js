204ed82f384501b583ff49233de631694fa8c506
Victor Mwenda <kiambi15@gmail.com>
2021-03-31 09:59:58 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect } from 'react';
+import axios from "axios";
+import {Row, Col, Card} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const fetchData = () => {
+      let url = "https://api.themoviedb.org/3/discover/movie?api_key=b1422e7b80cff3571090be90e6544244&sort_by=popularity.desc&page=1"
+      axios.get(url)
+          .then(response => {
+              console.log(response.data.results)
+              setMovies(response.data.results)
+          })
+          .catch(error => {
+              console.log(error)
+          });
+  }
+
+  useEffect(() => {
+     fetchData();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Row>
+          {movies.map((movie, index) => (
+                  <Col key={index} sm={4} md={4} lg={3}>
+                      <Card style={{ width: '18rem' }}>
+                          <Card.Img variant="top" src={"http://image.tmdb.org/t/p/w300//"+movie.poster_path} />
+                          <Card.Body>
+                              <Card.Title>{movie.title}</Card.Title>
+                              <Card.Text>
+                                  {movie.overview}
+                              </Card.Text>
+                          </Card.Body>
+                      </Card>
+                  </Col>
+              )
+          )}
+      </Row>
+  )
 }
 
 export default App;
